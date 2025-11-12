66acd5b69ffd011566987e2936707073e071ec4b
Lorcan Neary <lneary@bcis.co.uk>
2025-11-12 15:55:31 +0000

    simple script to obtain an image of wheat straw from a library

---
+++ utilities/straw_identifier/wheat_straw_test.py
@@ -0,0 +1,21 @@
+from duckduckgo_search import DDGS
+from fastdownload import download_url
+from fastai.vision.all import *
+from PIL import Image
+
+# Search for bird photos (DuckDuckGo)
+def search_images(term, max_images=1):
+    with DDGS() as ddgs:
+        return [r["image"] for r in ddgs.images(keywords=term, max_results=max_images)]
+
+urls = search_images('wheat straw photos', max_images=1)
+print("Image URL:", urls[0])
+
+# Download the image
+dest = 'wheat_straw.jpg'
+download_url(urls[0], dest, show_progress=False)
+
+# Open and display a thumbnail
+im = Image.open(dest)
+im.thumbnail((256, 256))
+im.show()
